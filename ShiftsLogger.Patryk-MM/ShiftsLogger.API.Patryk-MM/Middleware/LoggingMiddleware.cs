cc08b49c6ba1be59059364c419feab910c64598f
Patryk-MM <patryk.mikitka@gmail.com>
2026-05-02 22:12:30 +0200

    LoggingMiddleware refactored

--- ShiftsLogger.Patryk-MM/ShiftsLogger.API.Patryk-MM/Middleware/LoggingMiddleware.cs
+++ ShiftsLogger.Patryk-MM/ShiftsLogger.API.Patryk-MM/Middleware/LoggingMiddleware.cs
@@ -1,4 +1,4 @@
-﻿using System.Diagnostics;
+using System.Diagnostics;
 using System.Net;
 
 namespace ShiftsLogger.API.Patryk_MM.Middleware;
@@ -17,17 +17,22 @@ public class LoggingMiddleware {
         string method = context.Request.Method;
         string? ipAddress = context.Connection.RemoteIpAddress?.ToString();
         string traceId = context.TraceIdentifier;
+		string? userAgent = context.Request.Headers.UserAgent;
 
         var stopwatch = Stopwatch.StartNew();
 
-        _logger.LogInformation($"INCOMING [{traceId}]: {method} {path} from {ipAddress}");
+		_logger.LogInformation(
+		"INCOMING [{TraceId}]: {Method} {Path} from {UserAgent} | {IpAddress}",
+		 traceId, method, path, userAgent ?? "", ipAddress);
 
-        await _next(context);
+		await _next(context);
 
         stopwatch.Stop();
 
         int statusCode = context.Response.StatusCode;
 
-        _logger.LogInformation($"OUTGOING [{traceId}]: {method} {path} | Status: {statusCode} {(HttpStatusCode)statusCode} | Elapsed: {stopwatch.ElapsedMilliseconds} ms");
-    }
+		_logger.LogInformation(
+		"OUTGOING [{TraceId}]: {Method} {Path}\nStatus: {StatusCode} {StatusName}\nElapsed: {ElapsedMs} ms",
+		traceId, method, path, statusCode, (HttpStatusCode)statusCode, stopwatch.ElapsedMilliseconds);
+	}
 }
